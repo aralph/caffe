d1abae428645733e06e1dc2c508774ef824b67b3
Ralph Aeschimann <aralph@student.ethz.ch>
2016-07-11 19:23:14 +0200

    apply further changes according to commit "add crfrnn" on 12 April 2016 by bittnt/caffe - commit hash 67cddadcfef06961084b2a3bf3884017df8fb5c4 - add meanfield_iteration_layer - remove wrongly placed code from multi_stage_meanfiled_layer

--- include/caffe/layers/multi_stage_meanfield_layer.hpp
+++ include/caffe/layers/multi_stage_meanfield_layer.hpp
@@ -1,96 +1,18 @@
-#ifndef CAFFE_MULTI_STAGE_MEANFIELD_LAYER_HPP_
-#define CAFFE_MULTI_STAGE_MEANFIELD_LAYER_HPP_
-
+#ifndef CAFFE_MULTISTAGEMEANFIELD_LAYER_HPP_
+#define CAFFE_MULTISTAGEMEANFIELD_LAYER_HPP_
+#include <string>
+#include <utility>
 #include <vector>
 
-#include "caffe/filler.hpp"					// from .cpp of bittnt repo, unsure if needed explicitly
-#include "caffe/util/im2col.hpp"			// dito
-#include "caffe/util/tvg_util.hpp"			// dito
-#include "caffe/util/math_functions.hpp"	// dito
-
-#include "caffe/util/modified_permutohedral.hpp"	// added
-#include "caffe/layers/softmax_layer.hpp"			// added
-#include "caffe/layers/eltwise_layer.hpp"			// added
-#include "caffe/layers/split_layer.hpp"				// added
-#include <boost/shared_array.hpp>					// added
-
-
+#include "caffe/net.hpp"
 #include "caffe/blob.hpp"
 #include "caffe/layer.hpp"
 #include "caffe/proto/caffe.pb.h"
 
+#include "caffe/layers/meanfield_iteration_layer.hpp"
+#include "caffe/util/modified_permutohedral.hpp"
+#include <boost/shared_array.hpp>
 namespace caffe {
-
-template <typename Dtype>
-class MeanfieldIteration {
-
- public:
-  /**
-   * Must be invoked only once after the construction of the layer.
-   */
-  void OneTimeSetUp(
-      Blob<Dtype>* const unary_terms,
-      Blob<Dtype>* const softmax_input,
-      Blob<Dtype>* const output_blob,
-      const shared_ptr<ModifiedPermutohedral> spatial_lattice,
-      const Blob<Dtype>* const spatial_norm);
-
-  /**
-   * Must be invoked before invoking {@link Forward_cpu()}
-   */
-  virtual void PrePass(
-      const vector<shared_ptr<Blob<Dtype> > >&  parameters_to_copy_from,
-      const vector<shared_ptr<ModifiedPermutohedral> >* const bilateral_lattices,
-      const Blob<Dtype>* const bilateral_norms);
-
-  /**
-   * Forward pass - to be called during inference.
-   */
-  virtual void Forward_cpu();
-
-  /**
-   * Backward pass - to be called during training.
-   */
-  virtual void Backward_cpu();
-
-  // A quick hack. This should be properly encapsulated.
-  vector<shared_ptr<Blob<Dtype> > >& blobs() {
-    return blobs_;
-  }
-
- protected:
-  vector<shared_ptr<Blob<Dtype> > > blobs_;
-
-  int count_;
-  int num_;
-  int channels_;
-  int height_;
-  int width_;
-  int num_pixels_;
-
-  Blob<Dtype> spatial_out_blob_;
-  Blob<Dtype> bilateral_out_blob_;
-  Blob<Dtype> pairwise_;
-  Blob<Dtype> softmax_input_;
-  Blob<Dtype> prob_;
-  Blob<Dtype> message_passing_;
-
-  vector<Blob<Dtype>*> softmax_top_vec_;
-  vector<Blob<Dtype>*> softmax_bottom_vec_;
-  vector<Blob<Dtype>*> sum_top_vec_;
-  vector<Blob<Dtype>*> sum_bottom_vec_;
-
-  shared_ptr<SoftmaxLayer<Dtype> > softmax_layer_;
-  shared_ptr<EltwiseLayer<Dtype> > sum_layer_;
-
-  shared_ptr<ModifiedPermutohedral> spatial_lattice_;
-  const vector<shared_ptr<ModifiedPermutohedral> >* bilateral_lattices_;
-
-  const Blob<Dtype>* spatial_norm_;
-  const Blob<Dtype>* bilateral_norms_;
-
-};
-
 template <typename Dtype>
 class MultiStageMeanfieldLayer : public Layer<Dtype> {
 
@@ -103,8 +25,8 @@ class MultiStageMeanfieldLayer : public Layer<Dtype> {
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
 
-  virtual inline const char* type() const {
-    return "MultiStageMeanfield";
+  virtual inline const char* type() const { 
+    return "MultiStageMeanfield"; 
   }
   virtual inline int ExactNumBottomBlobs() const { return 3; }
   virtual inline int ExactNumTopBlobs() const { return 1; }
@@ -146,7 +68,6 @@ class MultiStageMeanfieldLayer : public Layer<Dtype> {
   boost::shared_array<float> bilateral_kernel_buffer_;
   vector<shared_ptr<ModifiedPermutohedral> > bilateral_lattices_;
 };
+}
 
-}  // namespace caffe
-
-#endif  // CAFFE_MULTI_STAGE_MEANFIELD_LAYER_HPP_
+#endif
